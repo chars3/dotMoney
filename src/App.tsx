9d8f21fc46eff338bbb1d6be78945fca1b1e2fe8
chars3 <charlesjoliveira@gmail.com>
2022-10-11 10:46:17 -0300

    upload files

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import Modal from 'react-modal'
+import { useState } from 'react';
+import { Dashboard } from "./components/Dashboard/Index";
+import { Header } from "./components/Header/Index";
+import { GlobalStyle } from "./styles/global";
+import { NewTransactionModal } from './components/NewTransactionModal/Index';
+import { TransactionsContext, TransactionsProvider } from './TransactionsContext';
+
+Modal.setAppElement('#root')
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+      setIsNewTransactionModalOpen(false)
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
+
+
